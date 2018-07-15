dcd39c3554ba0166bbf417050c9d14d2b065fc4b
Bassel Hossam <basselhossam.a@gmail.com>
2018-07-15 11:09:23 +0200

    Update database manager to handle exSummary error

--- app/DBAdmin/__init__.py
+++ app/DBAdmin/__init__.py
@@ -23,9 +23,12 @@ class DBManager:
         x = 0
         for article in articlesList:
             if 'exSummary' in article and 'ratio' in article and 'abSummary' in article:
-                posts_ref = self.ref.child('sources/' + article['newsApiID'] + '/articles')
-                posts_ref.push().set(article)
-                x = x+1
+                if article['exSummary'] == "[UNK] [UNK] says she will be despite to be despite to take action . she says she will be despite to take action against the next generation . she says she will be despite to get back to the next of the day .":
+                    print("An article exSummary failed")
+                else:
+                    posts_ref = self.ref.child('sources/' + article['newsApiID'] + '/articles')
+                    posts_ref.push().set(article)
+                    x = x+1
             else:
                 print("An article failed")
         return x
